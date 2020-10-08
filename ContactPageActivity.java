db8569c9a0807bc7e4f5f5bdc5930b3a7f9fddb5
NatHed <71409635+NatHed@users.noreply.github.com>
2020-10-08 19:01:21 -0400

    First draft of contact us page

---
+++ ContactPageActivity.java
@@ -0,0 +1,14 @@
+package nathaniel.hedman.sunshineboyscodes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_page);
+    }
+}
