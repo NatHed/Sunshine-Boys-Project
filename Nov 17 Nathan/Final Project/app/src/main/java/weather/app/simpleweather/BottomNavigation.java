01d4a15a3a41f40fde74753bd345c508884670a2
NatHed <71409635+NatHed@users.noreply.github.com>
2020-11-17 15:36:59 -0500

    On click events for bottom navigation

--- Nov 17 Nathan/Final Project/app/src/main/java/weather/app/simpleweather/BottomNavigation.java
+++ Nov 17 Nathan/Final Project/app/src/main/java/weather/app/simpleweather/BottomNavigation.java
@@ -1,7 +1,11 @@
 package weather.app.simpleweather;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,6 +18,26 @@ public class BottomNavigation extends AppCompatActivity {
 
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(BottomNavigation.this, "@string/homePage", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(BottomNavigation.this, HomeActivity.class));
+                        break;
+                    case R.id.action_contact:
+                        Toast.makeText(BottomNavigation.this, "@string/contactUs", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(BottomNavigation.this, ContactUsActivity.class));
+                        break;
+                    case R.id.action_about:
+                        Toast.makeText(BottomNavigation.this, "@string/abouUs", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(BottomNavigation.this, AboutActivity.class));
+                        break;
+                }
+                return true;
+            }
+        });
 
     }
 }
