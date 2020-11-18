e2078146b43f747cda5af7a4acc485f10de9bff8
Jp-CENG319 <72248164+Jp-CENG319@users.noreply.github.com>
2020-11-18 01:10:56 -0800

    REAL MILESTONE 2

--- MILESTONE 2 TO PRESENT/Final Project/app/src/main/java/weather/app/simpleweather/SettingsActivity.java
+++ MILESTONE 2 TO PRESENT/Final Project/app/src/main/java/weather/app/simpleweather/SettingsActivity.java
@@ -2,6 +2,8 @@ package weather.app.simpleweather;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +14,7 @@ import android.provider.Settings;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
@@ -20,6 +23,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class SettingsActivity extends AppCompatActivity {
 
     private static int DELAY = 4000;
+    private SwitchCompat darksw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +84,28 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
         askPermission(this);
+
+
+        darksw = findViewById(R.id.d_switch);
+
+
+        darksw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+
+                }else{
+
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+
+                }
+
+
+            }
+        });
 
 
     }
