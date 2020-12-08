06ef6dfe0b3e027f3ac4b3503f28af74fa6ce82a
Jp-CENG319 <72248164+Jp-CENG319@users.noreply.github.com>
2020-12-07 16:39:16 -0800

    Added Live weather data

--- LATEST SB WEATHER/SB Weather/sb weather/src/main/java/weather/app/simpleweather/Fragments/HomeFragment.java
+++ LATEST SB WEATHER/SB Weather/sb weather/src/main/java/weather/app/simpleweather/Fragments/HomeFragment.java
@@ -1,6 +1,7 @@
 package weather.app.simpleweather.Fragments;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -8,9 +9,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -18,6 +17,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import weather.app.simpleweather.Adapter;
@@ -36,28 +45,7 @@ public class HomeFragment extends Fragment {
     static View.OnClickListener myOnClickListener;
 
 
-    @Nullable
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-
-        String temp = getString(R.string.temperature);
-        String humid = getString(R.string.humidity);
-        String apressure = getString(R.string.windspeed);
-        String wspeed = getString(R.string.airpressure);
-
-        super.onCreate(savedInstanceState);
-        data = new ArrayList<Model>();
-        data.add(new Model
-                ("" + temp,"0"));
-        data.add(new Model("" + humid,"40%"));
-        data.add(new Model("" + apressure,"100kpa"));
-        data.add(new Model("" + wspeed,"10 Kmh"));
-
-
-        setHasOptionsMenu(true);
-
-
-    }
+    public String URL = "http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=23f04464b7119837cf1dc4fa8b39caa3&units=metric";
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -88,7 +76,7 @@ public class HomeFragment extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
-
+    //EDIT THIS VIEW
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -104,25 +92,97 @@ public class HomeFragment extends Fragment {
 
         String temp = getString(R.string.temperature);
         String humid = getString(R.string.humidity);
-        String apressure = getString(R.string.windspeed);
-        String wspeed = getString(R.string.airpressure);
+        String apressure = getString(R.string.airpressure);
+        String wspeed = getString(R.string.windspeed);
 
-        adapter = new Adapter(data);
-        recyclerView.setAdapter(adapter);
-        data = new ArrayList<Model>();
-        data.add(new Model
-                ("" + temp,"0"));
-        data.add(new Model("" + humid,"40%"));
-        data.add(new Model("" + apressure,"100kpa"));
-        data.add(new Model("" + wspeed,"10 Kmh"));
 
-        Adapter winfo = new Adapter(data, getActivity());
-        recyclerView.setAdapter(winfo);
+        new ReadJSONFeedTask().execute(URL);
 
         return view;
     }
 
+    public String readJSONFeed(String address) {
+        URL url = null;
+        try {
+            url = new URL(address);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        };
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            InputStream content = new BufferedInputStream(
+                    urlConnection.getInputStream());
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(content));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            urlConnection.disconnect();
+        }
+        return stringBuilder.toString();
+    }
 
 
+    private class ReadJSONFeedTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls) {
+            return readJSONFeed(urls[0]);
+        }
+        protected void onPostExecute(String result) {
+            try {
+                //JSONArray jsonArray = new JSONArray(result);
+                //Uncomment the two rows below to parse weather data from OpenWeatherMap
+                JSONObject weatherJson = new JSONObject(result);
+
+                //
+
+                JSONObject dataObject= weatherJson.getJSONObject("main");
+                JSONObject windsp_Object= weatherJson.getJSONObject("wind");
+
+
+               /* temperature.setText(dataObject.getString("temp"));
+                humidity.setText(dataObject.getString("humidity"));
+                wind_speed.setText(windsp_Object.getString("speed"));
+                air_pressure.setText(dataObject.getString("pressure"));*/
+
+                String temperature = (dataObject.getString("temp"));
+                String humidity = (dataObject.getString("humidity"));
+                String wind_speed = (windsp_Object.getString("speed"));
+                String pressure = (dataObject.getString("pressure"));
+
+                String temp = getString(R.string.temperature);
+                String humid = getString(R.string.humidity);
+                String apressure = getString(R.string.airpressure);
+                String wspeed = getString(R.string.windspeed);
+
+
+                adapter = new Adapter(data);
+                recyclerView.setAdapter(adapter);
+                data = new ArrayList<Model>();
+                data.add(new Model
+                        (temp,temperature));
+                data.add(new Model(humid,humidity));
+                data.add(new Model(apressure, pressure));
+                data.add(new Model(wspeed, wind_speed));
+
+                Adapter winfo = new Adapter(data, getActivity());
+                recyclerView.setAdapter(winfo);
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
