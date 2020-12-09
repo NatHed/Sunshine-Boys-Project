6a72ba5f46532e179dd0ae3fb948d41b80f2a9fa
Jp-CENG319 <72248164+Jp-CENG319@users.noreply.github.com>
2020-12-08 21:04:46 -0800

    updated code

---
+++ LATEST SB WEATHER NEW/SB Weather/sb weather/src/main/java/weather/app/simpleweather/fragment/AboutFragment.java
@@ -0,0 +1,70 @@
+package weather.app.simpleweather.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+import weather.app.simpleweather.MainActivity;
+import weather.app.simpleweather.Model;
+import weather.app.simpleweather.R;
+
+public class AboutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        getActivity().getMenuInflater().inflate(R.menu.menu,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        Fragment selectedFragment = null;
+        switch(item.getItemId()){
+
+            case R.id.logoutMenu: {
+
+                getActivity().finish();
+                startActivity(new Intent(getActivity(), MainActivity.class));
+                break;
+            }
+
+
+            case R.id.refreshMenu:{
+                selectedFragment = new HomeFragment();
+                break;
+            }
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_aboutus, container, false);
+        return view;
+    }
+}
