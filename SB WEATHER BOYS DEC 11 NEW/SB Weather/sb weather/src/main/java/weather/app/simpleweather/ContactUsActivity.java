de03dbf8d46d4f47bf3aa87959f8884ce5409188
Jp-CENG319 <72248164+Jp-CENG319@users.noreply.github.com>
2020-12-11 18:14:16 -0800

    Added Alert Dialog for contact us page

--- SB WEATHER BOYS DEC 11 NEW/SB Weather/sb weather/src/main/java/weather/app/simpleweather/ContactUsActivity.java
+++ SB WEATHER BOYS DEC 11 NEW/SB Weather/sb weather/src/main/java/weather/app/simpleweather/ContactUsActivity.java
@@ -1,11 +1,13 @@
 package weather.app.simpleweather;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -17,7 +19,7 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ContactUsActivity extends AppCompatActivity {
-
+    private final static int Request_code_permissions = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +29,17 @@ public class ContactUsActivity extends AppCompatActivity {
 
         if (ContextCompat.checkSelfPermission(ContactUsActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
 
-            ActivityCompat.requestPermissions(ContactUsActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+            permissions_dialog();
 
 
         }
+
+
+
+
+
+
+
 
 
         /*BottomNavigationView bottomNavigationView1 = (BottomNavigationView) findViewById(R.id.bottom_navi);
@@ -53,6 +62,54 @@ public class ContactUsActivity extends AppCompatActivity {
         });*/
 
 
+    }
+
+
+    public void permissions_dialog() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("For making phone calls from app")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            ActivityCompat.requestPermissions(ContactUsActivity.this, new String[]{Manifest.permission.CALL_PHONE}, Request_code_permissions);
+
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .create().show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(ContactUsActivity.this, new String[]{Manifest.permission.CALL_PHONE}, Request_code_permissions);
+
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        switch (requestCode) {
+            case Request_code_permissions:
+                for (int index = permissions.length - 1; index >= 0; --index) {
+                    if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
+                        // exit the app if one permission is not granted
+                        finish();
+                        return;
+                    }
+                }
+        }
     }
 }
 
